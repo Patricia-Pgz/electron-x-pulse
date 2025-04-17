f652336d68458f8841b99026b5ea887116dcb3a7
Patricia Pongratz <patricia.pongratz@stud-mail.uni-wuerzburg.de>
2025-04-17 19:15:03 +0200

    Add stb as submodule

---
+++ code/engine/include/engine/rendering/Texture.h
@@ -0,0 +1,20 @@
+//
+// Created by patri on 17.04.2025.
+//
+
+#ifndef TEXTURE_H
+#define TEXTURE_H
+
+namespace gl3 {
+namespace engine {
+namespace rendering {
+
+class Texture {
+
+};
+
+} // rendering
+} // engine
+} // gl3
+
+#endif //TEXTURE_H
---
+++ code/engine/src/rendering/Texture.cpp
@@ -0,0 +1,12 @@
+//
+// Created by patri on 17.04.2025.
+//
+
+#include "Texture.h"
+
+namespace gl3 {
+namespace engine {
+namespace rendering {
+} // rendering
+} // engine
+} // gl3
