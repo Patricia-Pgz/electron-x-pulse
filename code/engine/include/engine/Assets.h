b2d969ac121c1a264fac34ee01e60942eabc5a2d
Patricia-Pgz <patriciapongratz@web.de>
2025-09-14 16:12:00 +0200

    relative paths for executable/release

--- code/engine/include/engine/Assets.h
+++ code/engine/include/engine/Assets.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <filesystem>
+#include <Windows.h>
+#include <stdexcept>
 
 #define GET_STRING(x) #x
 #define GET_DIR(x) GET_STRING(x)
@@ -9,9 +11,22 @@ namespace fs = std::filesystem;
 namespace gl3::engine
 {
     /**
+     * @brief Gets the full path to the current executable.
+     */
+    inline fs::path getExecutablePath()
+    {
+        char buffer[MAX_PATH];
+        if (const DWORD length = GetModuleFileNameA(nullptr, buffer, MAX_PATH); length == 0 || length == MAX_PATH)
+        {
+            throw std::runtime_error("Failed to get executable path.");
+        }
+        return buffer;
+    }
+
+    /**
      * @brief Resolves the absolute path for an asset.
      *
-     * This utility joins the compile-time `ASSET_ROOT` macro with a relative asset path
+     * This utility joins the `ASSET_ROOT` macro with a relative asset path
      * and returns the canonical (absolute, normalized) path as a string.
      *
      * @param relativeAssetPath The relative path to the asset within the asset directory.
@@ -19,7 +34,8 @@ namespace gl3::engine
      */
     inline std::string resolveAssetPath(const fs::path& relativeAssetPath)
     {
-        auto mergedPath = (GET_DIR(ASSET_ROOT) / relativeAssetPath).make_preferred();
+        auto exeDir = getExecutablePath().parent_path();
+        auto mergedPath = (exeDir / ASSET_ROOT / relativeAssetPath).make_preferred();
         return canonical(mergedPath).string();
     }
 }
